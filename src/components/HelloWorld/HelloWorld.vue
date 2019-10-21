7c76a55cd58d05f4693608990374dd70ce4770ff
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-10-21 22:01:13 +0300

    hello vue

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <h2
+    v-on:click="paint"
+    v-bind:style="{ color: titleColor }"
+  >
+    {{ title }}
+  </h2>
+  <button v-on:click="reverse">Reverse title</button>
+</div>
+</template>
+<script>
+const randomColor = () => `#${(Math.random()*0xFFFFFF<<0).toString(16)}`;
+
+export default {
+  data: () => ({
+    title: "Hello Vue",
+    titleColor: randomColor()
+  }),
+  methods: {
+    reverse() {
+      this.title = this.title.split('').reverse().join('');
+    },
+    paint() {
+      this.titleColor = randomColor();
+    }
+  }
+};
+</script>
